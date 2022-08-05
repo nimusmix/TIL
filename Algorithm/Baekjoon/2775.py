0f9dc97c2f6c3edef819a91c1b21052316d940e4
nimusmix <nimusmix@gmail.com>
2022-08-05 17:43:45 +0900

    pull 받고 이어서 풀어야 함git add *

---
+++ Algorithm/Baekjoon/2775.py
@@ -0,0 +1,26 @@
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+
+    t_list = [[0] * 14 for _ in range(15)]
+    p = 0
+
+    for s in range(15):
+        for t in range(15):
+            
+
+
+
+
+
+
+
+
+'''
+3층 : 1명 5명 15명 35명
+2층 : 1명 4명 10명 20명
+1층 : 1명 3명 6명 10명
+0층 : 1명 2명 3명 4명
+'''
