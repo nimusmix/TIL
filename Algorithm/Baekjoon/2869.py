662d2a0469fce61b23703877f0bb1fc1ee60bf94
nimusmix <nimusmix@gmail.com>
2022-08-13 18:09:44 +0900

    p

--- Algorithm/Baekjoon/2869.py
+++ Algorithm/Baekjoon/2869.py
@@ -1,9 +1,3 @@
 A, B, V = map(int, input().split())
 
-# if (V-A) < (A-B):
-#     print((V-A) // (A-B) + 2)
-# else:
-#     print((V-A) // (A-B) + 1)
-    
-
 print((V-B)//(A-B) if (V-B) % (A-B) == 0 else (V-B)//(A-B)+1)
