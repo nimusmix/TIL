cbba941645e6f2d533408466fbed399f90b1f9f0
nimusmix <nimusmix@gmail.com>
2022-12-16 01:35:38 +0900

    [BOJ] 통계학 / 실버3 / 500ms

--- Algorithm/Baekjoon/2108.py
+++ Algorithm/Baekjoon/2108.py
@@ -1,33 +1,21 @@
 import sys
-from collections import defaultdict
-input = lambda: sys.stdin.readline().strip()
+from collections import Counter
+input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
 arr = []
-ans = [0] * 4
-cnt = defaultdict(int)
 
 for i in range(N):
     n = int(input())
     arr.append(n)
-    cnt[n] += 1
 
 
-ans[0] = round(sum(arr) / N)
+print(round(sum(arr) / N))
 
 arr.sort()
-ans[1] = arr[N // 2]
+print(arr[N // 2])
 
-cnt_list = []
-max_v = max(cnt.values())
-for k, v in cnt.items():
-    if v == max_v:
-        cnt_list.append(k)
-        
-cnt_list.sort()
-ans[2] = cnt_list[0] if len(cnt_list) == 1 else cnt_list[1]
+cnt = Counter(arr).most_common()
+print(cnt[1][0] if len(cnt) > 1 and cnt[0][1] == cnt[1][1] else cnt[0][0])
 
-ans[3] = abs(arr[-1] - arr[0])
-
-for i in ans:
-    print(i)
+print(abs(arr[-1] - arr[0]))
